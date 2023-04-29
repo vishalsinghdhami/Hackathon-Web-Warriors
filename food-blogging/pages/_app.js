2c3545f06aef925bacf0dc72bf071d3474500181
vishalbrdr <biradarvishal290@gmail.com>
2023-04-30 00:48:08 +0530

    small change

--- food-blogging/pages/_app.js
+++ food-blogging/pages/_app.js
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 
-import Layout from "../components/layout";
+import Layout from "@/components/layout";
 
 export default function MyApp({ Component, pageProps }) {
   return (
